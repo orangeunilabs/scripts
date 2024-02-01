511de7e57dcaff2a7c8412efab82375a5d9198ba
Henrik VT <22358337+henrikvtcodes@users.noreply.github.com>
2024-02-01 10:53:05 -0500

    Add logging to inittools.sh

--- debian/inittools.sh
+++ debian/inittools.sh
@@ -3,14 +3,25 @@
 
 apt-get update && apt-get upgrade -y
 
+echo "---------- UPDATE & UPGRADE COMPLETE ----------\n\n"
+
 apt-get install curl -y
+
+echo "---------- `curl` INSTALLED ----------\n\n"
 
 # Install speedtest-cli
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | bash 
 apt-get install speedtest -y 
 
+echo "---------- SPEEDTEST INSTALLED ----------\n\n"
+
 # Install tailscale
 curl -fsSL https://tailscale.com/install.sh | sh 
 
+echo "---------- TAILSCALE INSTALLED ----------\n\n"
+
 # Install iperf3
 apt-get install iperf3 -y 
+
+echo "---------- IPERF3 INSTALLED ----------"
+echo "---------- SCRIPT COMPLETE ----------"
